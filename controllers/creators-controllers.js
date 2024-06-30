765f5eebe4fcf2550f1acaf682bb63a180316fc8
Linh Le <lemylinh237@gmail.com>
2024-06-30 19:21:22 +0300

    Add creator

--- controllers/creators-controllers.js
+++ controllers/creators-controllers.js
@@ -1,8 +1,6 @@
-const { checkExistingUser, createCustomerQueryWithMedia, createCustomerQueryWithoutMedia, updateCustomerQuery, getSingleUserQuery } = require('../models/creator')
-const { checkMedia } = require('../middleware/check-relation')
-const { getSingleLanguageQuery } = require('../models/language')
-const { getSingleCountryQuery } = require('../models/country')
-
+const { checkExistingUser, createCustomerQuery, updateCustomerQuery, getSingleUserQuery, deleteUserQuery, allCreatorsQuery } = require('../models/creator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const HttpError = require('../models/http-error')
 require('dotenv').config()
 
@@ -16,7 +14,7 @@ const getUsers = async() => {
             'Fetching users failed, please try again later'
         )
     }
-    return users
+    return users.rows
 }
 
 const getSingleUser = async(userId) => {
@@ -32,8 +30,7 @@ const getSingleUser = async(userId) => {
     if (!user) {
       throw new HttpError('Could not find a user for the provided id.', 404)
     }
-  
-    return user
+    return user.rows
 }
 
 const updateUser = async(userId, firstName, lastName, imagePath, phone, countryId, languageId, aboutMe, x, linkedin, instagram, github, website) => {
@@ -55,7 +52,7 @@ const updateUser = async(userId, firstName, lastName, imagePath, phone, countryI
         throw new HttpError('Could not find image path for provided id.', 404)
     }
 
-    let userImage = user.image;
+    let userImage = user.rows[0].image;
     if (imagePath !== userImage) {
         // Delete existing image, then upload a new one
         try {
@@ -66,60 +63,46 @@ const updateUser = async(userId, firstName, lastName, imagePath, phone, countryI
         }
     }
 
-    const isMediaValid = checkMedia(x, linkedin, instagram, github, website)
-    const isValidLanguage = getSingleLanguageQuery(languageId)
-    const isValidCountry = getSingleCountryQuery(countryId)
-
-    if (!isValidLanguage.rows.length) {
-        throw new HttpError('Invalid Language Id')
-    }
-        
-    if (!isValidCountry.rows.length) {
-        throw new HttpError('Invalid Country Id')
-
-    }
-
     let createdUser
     try {
-        if (isMediaValid) {
-            createdUser = updateCustomerQueryWithMedia(x, linkedin, instagram, github, website,
-                firstName, lastName, email, countryId, languageId, password, image, aboutMe, phone)
-        } else {
-            createdUser = updateCustomerQueryWithoutMedia(firstName, lastName, email, countryId, languageId, password, image, aboutMe, phone)
-        }
+        createdUser = await updateCustomerQuery(x, linkedin, instagram, github, website,
+            firstName, lastName, countryId, languageId, image, aboutMe, phone, userId)
     } catch(err) {
         throw new HttpError('Something went wrong, could not update user', 500)
     }
-    return createdUser
+
+    let singleUser = await getSingleUser(userId)
+
+    return singleUser
 }
 
-const deleteUser = async(userId) => {
+const deleteUserById = async(userId) => {
     let user
     try {
-        
+        user = await getSingleUserQuery(userId)
     } catch(err) {
         throw new HttpError(
         'Fetching users failed, please try again later'
         )
     }
 
-    if(!user){
+    if(!user.rows.length){
         throw new HttpError('Could not find user for provided id.', 404)
     }
 
-    if (!user.image) {
+    if (!user.rows[0].image) {
         throw new HttpError('Could not find image path for provided id.', 404)
     }
 
     // Delete existing image
     try {
-        await deleteS3(user.image);
+        await deleteS3(user.rows[0].image);
     } catch(err) {
         throw new HttpError('Could not delete the user image, please try again', 500)
     }
 
     try {
-
+        await deleteUserQuery(userId)
     } catch(err) {
         throw new HttpError('Something went wrong, could not delete user', 500)
     }
@@ -127,7 +110,9 @@ const deleteUser = async(userId) => {
     return 'Deleted User'
 }
 
-const signup = async (firstName, lastName, email, countryId, languageId, password, image, aboutMe, phone) => {
+
+const signup = async (x, linkedin, instagram, github, website,
+    firstName, lastName, email, countryId, languageId, password, image, aboutMe, phone) => {
     let existingUser
     try {
         existingUser = await checkExistingUser(email)
@@ -150,27 +135,19 @@ const signup = async (firstName, lastName, email, countryId, languageId, passwor
     }
 
     let createdUser
-    const isValidLanguage = getSingleLanguageQuery(languageId)
-    const isValidCountry = getSingleCountryQuery(countryId)
-
-    if (!isValidLanguage.rows.length) {
-        throw new HttpError('Invalid Language Id')
-    }
-        
-    if (!isValidCountry.rows.length) {
-        throw new HttpError('Invalid Country Id')
-    }
-    
     try {
         createdUser = await createCustomerQuery(x, linkedin, instagram, github, website,
-            firstName, lastName, email, countryId, languageId, hashedPassword, image, aboutMe, phone)
+            firstName, lastName, email, countryId, languageId, password, image, aboutMe, phone)
     } catch(err) {
         throw new HttpError('Creating user failed, please try again')
     }
     
     let token
     try {
-      token = jwt.sign({ userId: createdUser.creator_id, email: createdUser.email }, process.env.TOKEN_KEY, {expiresIn: '1h'} )
+        token = jwt.sign({ userId: createdUser.rows[0].creator_id, email: createdUser.rows[0].email }, 
+            process.env.TOKEN_KEY, 
+            {expiresIn: '1h'} 
+        )
     } catch(err) {
       throw new HttpError(
         'Signing up failed, please try again later.',
@@ -178,13 +155,12 @@ const signup = async (firstName, lastName, email, countryId, languageId, passwor
       )
     }
 
-    return({ userId: createdUser.id, email: createdUser.email, token: token });
+    return({ userId: createdUser.rows[0].creator_id, email: createdUser.rows[0].email, token: token });
 }
-
 
 
 exports.getUsers = getUsers
 exports.getSingleUser = getSingleUser
 exports.updateUser = updateUser
-exports.deleteUser = deleteUser
+exports.deleteUserById = deleteUserById
 exports.signup = signup
