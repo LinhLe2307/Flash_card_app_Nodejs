51b0b55ffff4266ab50a77e53b1e7307e431da1c
Linh Le <lemylinh237@gmail.com>
2024-07-01 06:58:25 +0300

    Add env

--- postgresql/db.js
+++ postgresql/db.js
@@ -1,11 +1,12 @@
 const { Client } = require('pg')
+require('dotenv').config();
 
 const client = new Client({
-    user: 'linhle',
-    password: '1368',
-    port: 5432,
-    database: 'pernflashcard',
-    host: 'localhost'
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST
 })
 
 module.exports = client
